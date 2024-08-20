4f6ea98917848382ff5849884b49b1b05c57910f
unknown <nikitakumawat690@gmail.com>
2024-08-20 09:01:22 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="biodata-template">
+        <div className="left-section">
+          <img
+            src="https://i.pinimg.com/564x/7f/ab/14/7fab14c03b4249636a8e0fcdb4aeaa97.jpg"
+            alt="Profile"
+            className="profile-image"
+          />
+        </div>
+        <div className="right-section">
+          <h1>JOHN SMITH</h1>
+
+          <div className="about-me">
+            <h3>ABOUT ME</h3>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Date of Birth:</span>
+              <span style={{ marginLeft: '100px', color: 'black' }}>06-07-1993</span>
+            </p>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Height:</span>
+              <span style={{ marginLeft: '145px', color: 'black' }}>6'5"</span>
+            </p>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Education:</span>
+              <span style={{ marginLeft: '120px', color: 'black' }}>BSc.IT, MCA</span>
+            </p>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Hobbies:</span>
+              <span style={{ marginLeft: '130px', color: 'black' }}>Modeling, Singing</span>
+            </p>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Mobile no.:</span>
+              <span style={{ marginLeft: '110px', color: 'black' }}>+0 123-456-789</span>
+            </p>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Nationality:</span>
+              <span style={{ marginLeft: '110px', color: 'black' }}>Indian</span>
+            </p>
+            <p>
+              <span style={{ color: 'rgb(167, 105, 105)' }}>Income:</span>
+              <span style={{ marginLeft: '130px', color: 'black' }}>590k</span>
+            </p>
+          </div>
+
+          <div className="family-background">
+            <h3>FAMILY BACKGROUND</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+
+          <div className="expectation">
+            <h3>EXPECTATION</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
